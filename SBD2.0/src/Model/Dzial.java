9e0e09fcd34180c398a5d2b0b2c9ed5d65b31832
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-01-08 07:45:53 +0100

    Podstawowe mapowanie działów

---
+++ SBD2.0/src/Model/Dzial.java
@@ -0,0 +1,41 @@
+package Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DZIALY", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "ID_SZEFA")
+})
+public class Dzial {
+    @Id
+    @Column(name = "NR_DZIALU", nullable = false)
+    private Long id;
+    @Column(name = "NAZWA", nullable = false)
+    private String nazwa;
+    @Column(name = "ID_SZEFA")
+    private Long idSzefa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public Long getIdSzefa() {
+        return idSzefa;
+    }
+
+    public void setIdSzefa(Long idSzefa) {
+        this.idSzefa = idSzefa;
+    }
+}
