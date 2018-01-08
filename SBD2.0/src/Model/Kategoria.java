a09e9cff86069e585bce0c389599fb9f2ad0a075
Grzegorz Wilczyński <grzegorz.wilczynski@student.put.poznan.pl>
2018-01-08 07:52:55 +0100

    Podstawowe mapowanie kategorii

---
+++ SBD2.0/src/Model/Kategoria.java
@@ -0,0 +1,32 @@
+package Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Kategorie")
+public class Kategoria {
+    @Id
+    @Column(name = "NAZWA", nullable = false)
+    private String nazwa;
+    @Column(name = "OPIS", nullable = false)
+    private String opis;
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+}
